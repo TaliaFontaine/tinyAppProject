eba6a0f312cca23b87581f0f7342793b9f82f705
TaliaFontaine <taliafontaine1@gmail.com>
2019-02-16 05:29:32 +0000

    added a route for /urls and rendered the urls

--- express_server.js
+++ express_server.js
@@ -1,9 +1,10 @@
 var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
-
+//Set ejs as the view engine
 app.set("view engine", "ejs");
 
+//Object urlDatabase keeps track of all the URLs and their shortened forms
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -11,6 +12,13 @@ var urlDatabase = {
 
 app.get("/", (req, res) => {
   res.send("Hello!");
+});
+
+//Add route for /urls
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+
+  res.render("urls_index", templateVars);
 });
 //Added additional endpoint routes
 app.get("/urls.json", (req, res) => {
